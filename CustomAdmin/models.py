56cb8011c3b18c19cd3dee2aa61d59e0ddcc3093
Mukul Mantosh <mukulmantosh@yahoo.com>
2018-09-16 20:19:39 +0530

    Initial Commit

--- CustomAdmin/models.py
+++ CustomAdmin/models.py
@@ -1,52 +1,78 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
 from django.db import models
+import time
+import uuid
 
 
-class TimeStampModel(models.Model):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("Users must have an email address.")
+
+        if not password:
+            raise ValueError('Users must have an password')
+
+        user = self.model(
+            email=email
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email,
+                                password
+                                )
+        user.is_active = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=255, default='user')
+    last_name = models.CharField(max_length=255, default='user')
+    jwt_secret = models.UUIDField(default=uuid.uuid4, null=True)
+    email = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        abstract = True
-
-
-class CustomUser(TimeStampModel):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-    is_admin = models.BooleanField(default=False)
-    password = models.CharField(max_length=255)
+    objects = UserManager()
+    USERNAME_FIELD = "email"
 
     class Meta:
-        db_table = 'custom_user'
+        ordering = ["id"]
 
-    def __str__(self):
+    def get_full_name(self):
+        # The user is identified by their email address
         return self.first_name
 
-    def __repr__(self):
-        return self.__class__.__name__
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
 
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        if self.is_superuser:
+            return True
+        else:
+            return self.user_permissions.filter(codename=perm).exists()
 
-class Role(TimeStampModel):
-    name = models.CharField(unique=True, max_length=50)
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
 
-    class Meta:
-        db_table = "custom_role"
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
 
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return self.__class__.__name__
-
-
-class UserRole(TimeStampModel):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "custom_user_role"
-
-    def __str__(self):
-        return "{} has been assigned role {}.".format(self.user.first_name, self.role.name)
-
-    def __repr__(self):
-        return self.__class__.__name__
+        return self.is_superuser
--- SampelDemo/settings.py
+++ SampelDemo/settings.py
@@ -125,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = "CustomAdmin.User"
