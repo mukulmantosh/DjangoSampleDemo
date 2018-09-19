6318239f9fa3cf45191cddc522d058b0a90ffd71
Mukul Mantosh <mukulmantosh@yahoo.com>
2018-09-19 07:38:53 +0530

    Working on Companies API

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -6,10 +6,28 @@ app_name = "api/v1"
 
 urlpatterns = [
 
+    # SuperUser
     url(r'' + str(app_name) + '/super-admin-signup/$', v1_custom_admin_views.AdminSignupAPI.as_view(),
         name='super-admin-signup-v1'),
 
+    # Company
+    url(r'' + str(app_name) + '/company-signup/$', v1_companies_views.CompanySignupAPI.as_view(),
+        name='company-signup-v1'),
+
+    # Company Admin
     url(r'' + str(app_name) + '/company-admin-signup/$', v1_companies_views.CompanyAdminSignupAPI.as_view(),
         name='company-admin-signup-v1'),
+
+    # Employee Signup
+    url(r'' + str(app_name) + '/employee-signup/$', v1_companies_views.EmployeeSignupAPI.as_view(),
+        name='employee-signup-v1'),
+
+    # Employee Profile Update
+    url(r'' + str(app_name) + '/employee-profile-edit/$', v1_companies_views.EmployeeProfileEditAPI.as_view(),
+        name='employee-profile-edit-v1'),
+
+    # Employee Remove
+    url(r'' + str(app_name) + '/employee-remove/$', v1_companies_views.RemoveEmployeeAPI.as_view(),
+        name='employee-remove-v1'),
 
 ]
